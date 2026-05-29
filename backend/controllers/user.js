2927d7533096288fb6823ca9ce1f3356eaa08bfa
Floane VARATHEN <fvarathen@gmail.com>
2026-05-29 15:46:00 +0200

    fix: gestion erreur signup
    Gestion de l'erreur lorsque l'utilisateur s'inscrit sans mot de passe

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -3,6 +3,10 @@ const jwt = require('jsonwebtoken');
 const User = require('../models/User');
 
 exports.signup = (req, res, next) => {
+  if (!req.body.email || !req.body.password) {
+    return res.status(400).json({ error: 'Email et mot de passe requis' });
+  }
+
   bcrypt.hash(req.body.password, 10)
     .then(hash => {
       const user = new User({
@@ -10,7 +14,7 @@ exports.signup = (req, res, next) => {
         password: hash
       });
       user.save()
-        .then(() => res.status(201).json({ message: 'Utilisateur créé' }))
+        .then(() => res.status(201).json({ message: 'Utilisateur créé !' }))
         .catch(error => res.status(400).json({ error }));
     })
     .catch(error => res.status(500).json({ error }));
